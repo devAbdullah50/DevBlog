a6e5f3619b5836a81de0b17ebf82a9e1e9786296
Abdullah <devabdullah50@gmail.com>
2025-07-09 09:48:37 +0000

    Fix blog page bug

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,10 +3,10 @@ import { Metadata } from 'next'
 import Card from '@/components/Card'
 import Link from 'next/link'
 import BlogPostCard from '@/components/BlogPostCard'
-import { Data } from "@/lib/data";
+import { getPostBySlug, getAllPosts } from "@/lib/getBlogPosts";
 
 const blog = () => {
-    
+     const Data = getAllPosts()
     return (
         <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6 bg-gradient-to-r from-gray-900 to-black text-white">
             {Data.map((post) => (
